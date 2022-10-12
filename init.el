32d80751aac5608760f48d61d340d64964bb9540
Johan Bolmsjö <dev@johan.bitmaster.se>
2022-10-12 20:35:10 +0200

    Move custom variables to custom.el

--- init.el
+++ init.el
@@ -617,19 +617,5 @@ With argument, do this that many times."
 ;; Customized Variables
 ;; ----------------------------------------------------------------------------
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-agenda-files '("~/org/plan.org" "~/org/economy/subscriptions.org"))
- '(org-default-notes-file "~/org/notes.org")
- '(plantuml-default-exec-mode 'jar)
- '(plantuml-jar-path "/usr/share/plantuml/plantuml.jar")
- '(plantuml-server-url ""))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file)
