92efb0b91cde62d99a3ae28ea8a722a1799fbe88
Johan Bolmsjö <dev@johan.bitmaster.se>
2022-11-24 22:54:03 +0100

    Refine org mode publishing of README

--- publish.el
+++ publish.el
@@ -1,10 +1,23 @@
 (require 'ox-publish)
 
+;; Export options:
+;; - https://orgmode.org/manual/Export-Settings.html
+;; - https://orgmode.org/manual/Publishing-options.html
+
+(setq org-export-with-toc nil)
+(setq org-export-with-section-numbers 0)
+(setq org-export-headline-levels 4)
+(setq org-export-with-author nil)
+(setq org-export-time-stamp-file nil)
+(setq org-export-with-sub-superscripts nil)
+(setq org-export-with-date t)
+(setq org-html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"https://bitmaster.se/css/org-mini.css\" />")
+(setq org-html-metadata-timestamp-format "%Y-%m-%d")
+(setq org-html-validation-link nil)
+
 (setq org-publish-project-alist
       '(("emacs.d"
 	 :auto-sitemap nil
-	 :html-metadata-timestamp-format "%Y-%m-%d %H:%M"
-	 :html-validation-link nil
          :publishing-function org-html-publish-to-html
          :recursive nil
          :base-directory "./"
@@ -13,8 +26,6 @@
          :publishing-directory "html/")
 	("emacs.d-docs"
 	 :auto-sitemap nil
-	 :html-metadata-timestamp-format "%Y-%m-%d %H:%M"
-	 :html-validation-link nil
          :publishing-function org-html-publish-to-html
          :recursive nil
          :base-directory "docs/"
