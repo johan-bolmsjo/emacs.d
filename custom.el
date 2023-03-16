d9ac66d7b117987892c60d77f16c27174a94232a
Johan Bolmsjö <dev@johan.bitmaster.se>
2023-03-16 23:55:50 +0100

    Customize cmake-tab-width

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(cmake-tab-width 4)
  '(denote-directory "~/notes/")
  '(org-agenda-files '("~/org/plan.org"))
  '(org-default-notes-file "~/org/notes.org")
