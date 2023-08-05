ff8c5d12b9ba0f2f481f32d661b91eb24197b9d5
Johan Bolmsjö <dev@johan.bitmaster.se>
2023-08-05 08:45:52 +0200

    Add interactive functions for bigger fonts

--- fonts.el
+++ fonts.el
@@ -1,3 +1,27 @@
+(defun my/set-font (height)
+  "Set default fonts with the specified height"
+  (set-face-attribute 'default nil :family "Go Mono" :height height)
+  (set-face-attribute 'variable-pitch nil :family "Go" :height height))
+
+(defun my/font+0 ()
+  "Set default fonts"
+  (interactive)
+  (my/set-font 90))
+
+(defun my/font+1 ()
+  "Set default fonts with increased size."
+  (interactive)
+  (my/set-font 100))
+
+(defun my/font+2 ()
+  "Set default fonts with increased size."
+  (interactive)
+  (my/set-font 110))
+
+(defun my/font+3 ()
+  "Set default fonts with increased size."
+  (interactive)
+  (my/set-font 120))
+
 (when (display-graphic-p)
-  (set-face-attribute 'default nil :family "Go Mono" :height 90)
-  (set-face-attribute 'variable-pitch nil :family "Go" :height 90))
+  (my/font+0))
