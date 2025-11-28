340d30ababae1f664e7c08a505dc91f9354f4f5f
Johan Bolmsjö <dev@johan.bitmaster.se>
2025-11-28 16:59:43 +0100

    wider bookmark list name column

--- custom.el
+++ custom.el
@@ -12,6 +12,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(bookmark-bmenu-file-column 60)
  '(cmake-tab-width 4)
  '(denote-directory "~/notes/")
  '(denote-excluded-files-regexp "/attachments/")
