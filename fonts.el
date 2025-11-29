eff02b10ca05ccc8d5bf7341495c9ed46df344e0
Johan Bolmsjö <dev@johan.bitmaster.se>
2025-11-29 23:59:42 +0100

    reduce granularity of font size adjustment

--- fonts.el
+++ fonts.el
@@ -3,25 +3,40 @@
   (set-face-attribute 'default nil :family "Go Mono" :height height)
   (set-face-attribute 'variable-pitch nil :family "Go" :height height))
 
-(defun my/font+0 ()
-  "Set default fonts"
+(defun my/font+00 ()
+  "Set default font with relative size 0.0"
   (interactive)
   (my/set-font 90))
 
-(defun my/font+1 ()
-  "Set default fonts with increased size."
+(defun my/font+05 ()
+  "Set default font with relative size 0.5"
+  (interactive)
+  (my/set-font 95))
+
+(defun my/font+10 ()
+  "Set default font with relative size 1.0"
   (interactive)
   (my/set-font 100))
 
-(defun my/font+2 ()
-  "Set default fonts with increased size."
+(defun my/font+15 ()
+  "Set default font with relative size 1.5"
+  (interactive)
+  (my/set-font 105))
+
+(defun my/font+20 ()
+  "Set default font with relative size 2.0"
   (interactive)
   (my/set-font 110))
 
-(defun my/font+3 ()
-  "Set default fonts with increased size."
+(defun my/font+25 ()
+  "Set default font with relative size 2.5"
+  (interactive)
+  (my/set-font 115))
+
+(defun my/font+30 ()
+  "Set default font with relative size 3.0"
   (interactive)
   (my/set-font 120))
 
 (when (display-graphic-p)
-  (my/font+0))
+  (my/font+00))
