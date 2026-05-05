2c688f5ed72290dbcbdf22f9b7144aa7f94ed853
Johan Bolmsjö <dev@johan.bitmaster.se>
2026-05-05 21:10:52 +0200

    Tweak avy key bindings

--- custom.el
+++ custom.el
@@ -23,17 +23,16 @@
  '(org-link-frame-setup
    '((vm . vm-visit-folder-other-frame)
      (vm-imap . vm-visit-imap-folder-other-frame)
-     (gnus . org-gnus-no-new-news)
-     (file . find-file)
+     (gnus . org-gnus-no-new-news) (file . find-file)
      (wl . wl-other-frame)))
  '(org-plantuml-jar-path "/usr/share/plantuml/plantuml.jar")
  '(org-startup-folded 'nofold)
  '(plantuml-default-exec-mode 'jar)
  '(plantuml-jar-path "/usr/share/plantuml/plantuml.jar")
  '(plantuml-server-url "")
- '(split-height-threshold 72)
- '(split-width-threshold 180)
- '(xref-search-program 'ripgrep))
+ '(split-height-threshold 80)
+ '(split-width-threshold nil)
+ '(xref-search-program 'ripgrep)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
